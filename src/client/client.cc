ce6a562282cd03b45d5163b44ac9058c6ddb81c5
Michael Whittaker <mjw297@cornell.edu>
2017-02-05 01:39:19 +0000

    Added Google's glog logging library.

--- src/client/client.cc
+++ src/client/client.cc
@@ -4,6 +4,7 @@
 
 #include <zmq.hpp>
 
+#include "glog/logging.h"
 #include "proto/api.pb.h"
 #include "zeromq_util/util.h"
 
@@ -13,14 +14,20 @@ void prompt() { std::cout << "> " << std::flush; }
 
 }  // namespace
 
-int main() {
+int main(int, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+
   zmq::context_t context(1);
   zmq::socket_t socket(context, ZMQ_REQ);
-  socket.connect("tcp://localhost:5555");
+  const std::string address = "tcp://localhost:5555";
+  socket.connect(address);
+  LOG(INFO) << "Client connected to " << address;
 
   prompt();
   for (std::string line; std::getline(std::cin, line); prompt()) {
     Request request;
+    long x = std::stol(line);
+    CHECK_GT(x, 0) << "Cannot factor non-positive value.";
     request.set_x(std::stol(line));
     send_proto(request, &socket);
 
